194f1cc735d50351bef6b89ff60d9c5eec8aaa23
Casey <caseyderiso@gmail.com>
2020-10-25 14:36:57 -0500

    refactored getuserrepos to accept a parameter and display any usernames list of repos

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -1,9 +1,12 @@
-const getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+const getUserRepos = function (user) {
+  // format the github api url
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
     });
   });
 };
 
-getUserRepos();
+getUserRepos("caseyderiso");
